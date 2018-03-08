8a103955b2813abadddc1cb93156a67bb365f709
Rich Benner <richhunterbenner@gmail.com>
2018-03-08 09:46:46 +0000

    Update Environmment.sql

--- Environmment.sql
+++ Environmment.sql
@@ -1,3 +1,17 @@
+/*************************************************************************************************
+Creator:    Rich Benner of RichBenner.com
+File:       Environment.sql
+Summary:    Check basic information of a server
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*************************************************************************************************/
+
 IF OBJECT_ID('tempdb..#Environment') IS NOT NULL DROP TABLE #Environment
 CREATE TABLE #Environment (ID int, Setting varchar(50), Result nvarchar(50))
 
